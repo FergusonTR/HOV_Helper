e189b599e48e43515b10a31097f31ad70fc83eac
FergusonTR <fergusontr@gmail.com>
2015-06-05 00:22:35 -0400

    added classes for Event and EventList and a EventTest

---
+++ app/src/main/java/sweng500team2summer15/hov_helper/Event.java
@@ -0,0 +1,29 @@
+package sweng500team2summer15.hov_helper;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Terry on 6/4/2015.
+ */
+public class Event {
+    int loginId;
+    int eventId;
+    int numberSeats;
+
+    Date startTime;
+
+    String frequency;
+    String eventType;
+    String startLocation;
+    String endLocation;
+
+    //SimpleDateFormat format = new SimpleDateFormat("");
+
+    public int create(int loginId) {
+
+        return 0;
+    }
+
+
+}
