1832a0e69ad0d2c77922ed303ae42fb192568a9b
sweng581team2 <steve4721@hotmail.com>
2015-06-06 10:58:12 -0500

    added maps activity

---
+++ app/src/main/java/sweng500team2summer15/hov_helper/Map.java
@@ -0,0 +1,79 @@
+package sweng500team2summer15.hov_helper;
+
+/**
+ * Created by Steve on 6/6/2015.
+ */
+public class Map {
+
+    /**
+     * Get the current location string (street, city, state, zip code)
+     * @return
+     */
+    public String getCurrentLocation()
+    {
+        String currentLocation = "";
+        // TODO:
+
+        return currentLocation;
+    }
+
+    public String getCurrentLocationStreetAddress()
+    {
+        String currentStreetAddress = "";
+        // TODO:
+
+        return currentStreetAddress;
+    }
+
+
+    public String getCurrentLocationCity()
+    {
+        String currentCity = "";
+        // TODO:
+
+        return currentCity;
+    }
+
+    public String getCurrentLocationState()
+    {
+        String currentCity = "";
+        // TODO:
+
+        return currentCity;
+    }
+
+    public String getCurrentLocationZipCode()
+    {
+        String currentZipcode = "";
+        // TODO:
+
+        return currentZipcode;
+    }
+
+    public double getCurrentLatitude()
+    {
+        double currentLat = 0.0;
+        // TODO:
+        return currentLat;
+    }
+
+    public double getCurrentLongitude()
+    {
+        double currentLon = 0.0;
+        //TODO:
+        return currentLon;
+    }
+
+    public void setEventStartLocation(String startLocation)
+    {
+        // TODO:
+    }
+
+    public void setEventEndLocation(String endLocation)
+    {
+        // TODO:
+    }
+
+
+
+}
