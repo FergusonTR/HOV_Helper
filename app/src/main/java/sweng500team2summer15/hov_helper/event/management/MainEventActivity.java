c49fcec3e61403b772f19cf714e749988cf96d22
sweng581team2 <steve4721@hotmail.com>
2015-08-05 00:53:05 -0500

    Merge branch 'master' of https://github.com/FergusonTR/HOV_Helper
    Conflicts:
    	app/src/main/AndroidManifest.xml
    	app/src/main/res/values/strings.xml

--- app/src/main/java/sweng500team2summer15/hov_helper/event/management/MainEventActivity.java
+++ app/src/main/java/sweng500team2summer15/hov_helper/event/management/MainEventActivity.java
@@ -5,9 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -16,15 +13,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 import sweng500team2summer15.hov_helper.Account.ChangePasswordActivity;
 import sweng500team2summer15.hov_helper.Account.SignInActivity;
 import sweng500team2summer15.hov_helper.Profile.ProfileManagement;
-import sweng500team2summer15.hov_helper.eventdisplay.ListEventsFragment;
-import sweng500team2summer15.hov_helper.map.MapFragment;
 import sweng500team2summer15.hov_helper.map.MapsActivity;
 import sweng500team2summer15.hov_helper.R;
 
 public class MainEventActivity extends AppCompatActivity {
+    private ArrayList<Event> arrayListOfEvents = new ArrayList<Event>();
 
     Button btnNewEvent;
     Button btnReadEvent;
@@ -34,9 +32,47 @@ public class MainEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_event);
+
+        // TODO: Remove Events Below after list of events query implemented. For Testing only
+        ArrayList<Event> myList = new ArrayList<Event>();
+        // populate with dummy data
+        Event e1 = new Event();
+        e1.eventId = 1;
+        e1.eventType = "Ride";
+        e1.startLatitude = 40.82;
+        e1.startLongitude = -77.8561126;
+        e1.endLatitude = 40.8122837;
+        e1.endLongitude = -77.8561126;
+        e1.numberAvailable = 4;
+        myList.add(e1);
+        Event e2 = new Event();
+        e2.eventId = 2;
+        e2.eventType = "Drive";
+        e2.startLatitude = 40.83;
+        e2.startLongitude = -77.8561126;
+        e2.endLatitude = 40.8122837;
+        e2.endLongitude = -77.8561126;
+        e2.numberAvailable = 2;
+        myList.add(e2);
+
+        // TODO: remove below line. This is an example of how another activity would pass in an array of events
+        getIntent().putExtra("eventList", myList);
+
         //Swipe pages
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        viewPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+        SwipableTabAdapter tabAdapter = new SwipableTabAdapter(getSupportFragmentManager());
+        tabAdapter.setListTabTitle("List My Events");
+        tabAdapter.setMapTabTitle("Map My Events");
+        viewPager.setAdapter(tabAdapter);
+
+        // get arraylist of events passed in to populate tab list and tab map (Note: If populating list in this activity, remove below line)
+        this.arrayListOfEvents = (ArrayList<Event>)getIntent().getSerializableExtra("eventList");
+        if (this.arrayListOfEvents == null)
+        {
+            // create empty list
+            this.arrayListOfEvents = new ArrayList<Event>();
+        }
+        tabAdapter.setEvents(this.arrayListOfEvents);
 
         //Buttons
         btnNewEvent = (Button) findViewById(R.id.btnCreateEventScrn);
@@ -72,43 +108,6 @@ public class MainEventActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });*/
-    }
-
-    /**
-     * This Class allows me to manage multiple swipe pages
-     */
-    public class MyAdapter extends FragmentPagerAdapter{
-
-        public MyAdapter(FragmentManager fm){
-            super(fm);
-
-        }
-        @Override
-        public Fragment getItem(int arg0){
-            switch (arg0) {
-                case 0:
-                    return new MapFragment();
-                case 1:
-                    return new ListEventsFragment();
-                default:
-                    break;
-                          }
-            return null;
-            }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            if(position==0) {
-                return "Map Events";
-            } else {
-                return "List Events";
-            }
-        }
-
-        public int getCount() {
-            return 2;
-        }
-
     }
 
     // ACTION BAR ITEMS
